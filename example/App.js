0e3113df0a8326ce63e2fcae34e27a6be1ec2e4b
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2020-08-05 01:29:53 +0300

    Update packages in example

--- example/App.js
+++ example/App.js
@@ -1,66 +1,56 @@
-import React, { Component } from 'react'
-import { StyleSheet, StatusBar, View, Text, ScrollView } from 'react-native'
-import TextInput from 'react-native-material-textinput'
+import React, { useState } from "react";
+import { StyleSheet, StatusBar, View, Text, ScrollView } from "react-native";
+import TextInput from "react-native-material-textinput";
 
-export default class App extends Component {
-  state = {
-    productName: '',
-    price: '',
-    multilineText:
-      'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.'
-  }
+export default function App() {
+  const [productName, setProductName] = useState("");
+  const [price, setPrice] = useState("");
+  const [multilineText, setMultilineText] = useState(
+    "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers."
+  );
 
-  render() {
-    let { productName, price, multilineText } = this.state
+  const handleProductNameChange = (value) => setProductName(value);
+  const handlePriceChange = (value) => setPrice(value);
+  const handleMultilineTextChange = (value) => setMultilineText(value);
 
-    return (
-      <View style={styles.layout}>
-        <StatusBar backgroundColor="#1a237e" barStyle="light-content" />
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Material Design Text Fields</Text>
-        </View>
-        <ScrollView contentContainerStyle={styles.container}>
-          <TextInput
-            label="Product name"
-            value={productName}
-            onChangeText={productName => this.setState({ productName })}
-          />
-          <TextInput
-            label="Price"
-            placeholder="$5,999.00"
-            value={price}
-            onChangeText={price => this.setState({ price })}
-          />
-          <TextInput
-            label="The Matrix"
-            value={multilineText}
-            onChangeText={multilineText => this.setState({ multilineText })}
-            multiline
-            minHeight={150}
-          />
-        </ScrollView>
+  return (
+    <View style={styles.layout}>
+      <StatusBar backgroundColor="#1a237e" barStyle="light-content" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Material Design Text Fields</Text>
       </View>
-    )
-  }
+      <ScrollView contentContainerStyle={styles.container}>
+        <TextInput label="Product name" value={productName} onChangeText={handleProductNameChange} />
+        <TextInput label="Price" placeholder="$5,999.00" value={price} onChangeText={handlePriceChange} />
+        <TextInput
+          label="The Matrix"
+          value={multilineText}
+          onChangeText={handleMultilineTextChange}
+          multiline
+          minHeight={150}
+        />
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   layout: {
     flex: 1,
-    backgroundColor: 'white'
+    backgroundColor: "white",
   },
   header: {
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
     height: 100,
     padding: 16,
-    backgroundColor: '#3f51b5'
+    backgroundColor: "#3f51b5",
   },
   headerText: {
-    color: 'white',
+    color: "white",
     fontSize: 20,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   container: {
-    padding: 16
-  }
-})
+    padding: 16,
+  },
+});
