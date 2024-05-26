d2a893a81147f2cdfbf6bd03959140d101565686
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2024-05-26 04:45:27 +0400

    Add export types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export { default } from './components/Input'
+
+export type {
+  InputProps as TextInputProps,
+  LabelProps as TextInputLabelProps,
+  UnderlineProps as TextInputUnderlineProps,
+  PlaceholderProps as TextInputPlaceholderProps,
+  ErrorHelperProps as TextInputErrorHelperProps
+} from './components/types'
